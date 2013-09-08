b86655f486b59fd63c281d9a3ac59fbb9ec615bf
Karan Goel <karan@goel.im>
2013-09-08 09:28:54 +0530

    Some improvements

--- ndt.py
+++ ndt.py
@@ -20,28 +20,21 @@ print '[*] Logging in...'
 r.login(username, password)
 print '[*] Login successful...\n'
 
-dae = r.get_subreddit('DAE') # praw.objects.Subreddit
+dae = r.get_subreddit('DoesAnybodyElse') # praw.objects.Subreddit
 
 print '[*] Getting submissions...\n'
 submissions = dae.get_new(limit=None) # get new posts
 
-already_done = []
-
-while True:
-    for submission in submissions:
-        if submission.id not in already_done:
-            created = time.strftime('%Y-%m-%d %H:%M:%S', # epoch to string
-                                    time.localtime(submission.created))
-            created = datetime.strptime(created,
-                                        '%Y-%m-%d %H:%M:%S') # str to datetime
-            if ((created - datetime.now()).total_seconds() > 604800):
-                # post is older than a week
-                if (submission.num_comments == 0) or (submission.ups == submission.downs):
-                    print '[*] Thread: %s' % submission.title
-                    # 0 comments or 0 votes
-                    comment = submissions.add_comment('Nope, it\'s just you.')
-                    print '\tComment %s added' % comment.permalink
-                    
-                    print '[*] Sleeping for 10 minutes\n'
-                    time.sleep(600) # sleep for 10 mins
-        already_done.append(submission.id)
+for submission in submissions:
+    print '[*] Thread: %s' % submission.title
+    created = datetime.fromtimestamp(submission.created_utc) # epoch to datetime
+    diff = (created - datetime.now()).total_seconds()
+    if (diff >= 604800): # post is older than a week
+        if (submission.score == 0) and (not submission.hidden):
+            # score = 0
+            comment = submissions.add_comment('Nope, it\'s just you.')
+            print '\tComment %s added' % comment.permalink
+            submission.hide()
+            #print '[*] Sleeping for 10 minutes\n'
+            #time.sleep(600) # sleep for 10 mins
+    time.sleep(2) # to comply with rate limit
